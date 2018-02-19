d22e7ee2f70cea95cdb26485f14fc533b2fe7f4e
TsarpDev <36573435+TsarpDev@users.noreply.github.com>
2018-02-19 16:18:44 +0200

    Changed canti_dist on matlab

--- matlab/canti_dist.m
+++ matlab/canti_dist.m
@@ -1,8 +1,8 @@
 clear
 
-
-syms x a b c d e L Es I kappa P g
-w(x) =  g*x^5 +a*x^4+b*x^3+c*x^2+d*x+e;
+% shape function and forces
+syms x a b c d e L Es I kappa P g m
+w(x) = m*x^6+ g*x^5 +a*x^4+b*x^3+c*x^2+d*x+e;
 
 w1(x)= diff(w,x);
 
@@ -16,7 +16,21 @@ M(x) = Es*I*k(x);
 
 V(x) =  -Es*I*w3(x);
 
-p_dist(x) = P*x/L
+
+% initialize
+syms N;
+N=9;
+dx = L/N;
+
+p_pt=[];
+w_pt=[];
+for i=0:dx:L
+  p_pt =[ p_pt, [P*i/L]];
+  eval(subs(w,i));
+  w_pt =[ w_pt, [ans]];
+end
+
+
 
 %boundary conditions
 w0 = w(0);
@@ -30,7 +44,7 @@ ML = M(L);
 eq3 = eval(ML) == 0;
 
 VL = V(L);
-eq4 = VL == 0;
+eq4 = VL == P;
 
 
 [A,B] = equationsToMatrix([eq1, eq2, eq3, eq4], [b, c, d, e]);
@@ -47,35 +61,49 @@ e = X(4)
 rot(x) = diff(w,x);
 rot0 = rot(0);
 
-U(a,g)=(1/2)*int(M*k,[0,L])+(1/2)*kappa*rot0^2
-U(a,g) = subs(U)
-Omega(a,g)= -int(p_dist*w,[0,L]);
-Omega(a,g) = subs(Omega)
+U(a,g,m)=(1/2)*int(M*k,[0,L])+(1/2)*kappa*rot0^2
+U(a,g,m) = subs(U)
 
-V(a,g)=U(a,g)+Omega(a,g)
+Omega(a,g,m)=0*a*g;
+for i=1:(N+1)
+Omega(a,g,m)= Omega(a,g,m)-p_pt(i)*w_pt(i);
+end
 
-dVa(a,g) = subs(diff(V,a))
-dVg(a,g) = subs(diff(V,g))
+Omega(a,g,m) = subs(Omega)
+
+V(a,g,m)=U(a,g,m)+Omega(a,g,m)
+
+dVa(a,g,m) = subs(diff(V,a))
+dVg(a,g,m) = subs(diff(V,g))
+dVm(a,g,m) = subs(diff(V,m))
 
 eq5 = dVa == 0
 eq6 = dVg == 0
+eq7 = dVm == 0
 
-[A,B] = equationsToMatrix([eq5, eq6], [a,g]);
+[A,B] = equationsToMatrix([eq5, eq6, eq7], [a,g,m]);
 X = linsolve(A,B);
 a = X(1);
 g = X(2);
+m = X(3);
 wL = subs(w(L));
 delta = subs(wL);
 delta = simplify(delta)
 
 
+
+syms d_;
+eqK = delta == d_;
+kappa_ = solve(eqK,kappa)
+
+
 Es = 210e6;
 L = 9.0;
 I = 0.000432;
-P = 15.0;
-d = 1.2724;
+P = 2.0;
+d = 0.0172911;
 
-% d_calc = eval(subs(delta,3000e30))
+ d_calc = eval(subs(delta,459680))
 % delta(kappa) = eval(subs(vpa(delta)));
 % 
 % [N,D] = numden(delta);
@@ -89,7 +117,7 @@ zer = simplify(zer*D)
 
 f = @(kappa)[eval(zer)];
 
-sol = fzero(f,100.0)
+sol = fzero(f,1000000.0)
 
 %kappa = vpa(subs(kappa));
 
